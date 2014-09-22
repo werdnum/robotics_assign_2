31e4684d2c29a3682ec75383ffdd2c79403c3f21
Andrew Garrett <andrew@andrewjgarrett.com>
2014-09-22 22:40:20 +1000

    * Add package files. * Update block detector to be more accurate

--- opencv/block_detector.py
+++ opencv/block_detector.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+
 import sys
 import rospy
 import cv2
@@ -8,12 +9,18 @@ from cv_bridge import CvBridge, CvBridgeError
 
 colourMap = {
     'red' : [ [160, 128, 32], [180, 255, 255] ], #170
-    'purple' : [ [125, 32, 32], [145, 255, 255] ], # 115
-    'green' : [ [45, 64, 32], [70, 255, 255] ], #58
-    'yellow' : [ [25, 150, 32], [35, 255, 255] ], # 34
+    'purple' : [ [120, 32, 32], [150, 255, 255] ], # 115
+    'green' : [ [35, 64, 32], [70, 255, 255] ], #58
+    'yellow' : [ [25, 96, 128], [45, 255, 255] ], # 34
     'blue' : [ [95, 150, 32], [110, 255, 255] ], # 104
-    'orange' : [ [0, 150, 32], [20, 255, 255] ], # 9
+    'orange' : [ [0, 160, 128], [20, 255, 255] ], # 9
 }
+
+## Hierarchy array constants
+HIERARCHY_NEXT = 0
+HIERARCHY_PREV = 1
+HIERARCHY_FIRST_CHILD = 2
+HIERARCHY_PARENT = 3
 
 class block_detector:
     def __init__(self):
@@ -33,27 +40,14 @@ class block_detector:
         coloursOfInterestMask = np.zeros( (hsvImage.shape[0], hsvImage.shape[1]), np.uint8 )
 
         ## Create a mask that matches any of the colours of interest
-        drawRects = []
         drawContours = []
         for colourName, colourBounds in colourMap.iteritems():
-            lowerBound = np.array( colourBounds[0] )
-            upperBound = np.array( colourBounds[1] )
-            colourMask = cv2.inRange( hsvImage, lowerBound, upperBound )
+            (colourMask, newContours) = self.processColour( hsvImage, colourName, colourBounds )
             coloursOfInterestMask = cv2.bitwise_or( coloursOfInterestMask, colourMask )
-            colourContours, hierarchy = cv2.findContours( colourMask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE )
-
-            for cnt in colourContours:
-                approx = cv2.approxPolyDP( cnt, 0.05 * cv2.arcLength( cnt, True), True )
-                approxArea = cv2.contourArea( approx )
-                
-                if len(approx) == 4 and approxArea > 100 and cv2.isContourConvex( approx ):
-                    drawContours.append( approx )
+            drawContours.extend( newContours )
 
         # Bitwise-AND mask and original image
         res = cv2.bitwise_or( cv_image, np.ones( cv_image.shape, np.uint8 ), mask=coloursOfInterestMask )
-        for rect in drawRects:
-            x, y, w, h = rect
-            cv2.rectangle( res, (x, y), (x+w, y+h), (255,0, 0), 2 )
             
         for cnt in drawContours:
             cv2.drawContours( res, drawContours, -1, (0,255,255), 2 )
@@ -61,6 +55,42 @@ class block_detector:
         resultImage = self.bridge.cv2_to_imgmsg( res, encoding="passthrough" )
         
         self.image_pub.publish( resultImage )
+
+    def processColour( self, hsvImage, colourName, colourBounds ):
+        lowerBound = np.array( colourBounds[0] )
+        upperBound = np.array( colourBounds[1] )
+        colourMask = cv2.inRange( hsvImage, lowerBound, upperBound )
+        colourContours, hierarchy = cv2.findContours( colourMask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE )
+        drawContours = []
+
+        for index in range( 0, len( colourContours ) ):
+            if self.isBlock( colourContours, hierarchy, index ):
+                drawContours.append( colourContours[index] )
+
+        return (colourMask, drawContours)
+
+    def isBlock( self, contours, hierarchy, index ):
+        contour = contours[index]
+        ## Simple screen: throw out non-squares
+        if not self.isSquare( contour ):
+            return False
+
+        childIndex = hierarchy[0][index][HIERARCHY_FIRST_CHILD]
+        while ( childIndex >= 0 ):
+            child = contours[childIndex]
+            if ( self.isSquare( child ) ):
+                childArea = cv2.contourArea( child )
+                return True
+            
+            childIndex = hierarchy[0][childIndex][HIERARCHY_NEXT]
+        
+        return False
+
+    def isSquare( self, contour ):
+        approxShape = cv2.approxPolyDP( contour, 0.01 * cv2.arcLength( contour, True), True )
+        area = cv2.contourArea( approxShape )
+        return ( ( len( approxShape ) == 4 ) and ( area > 100 ) and ( cv2.isContourConvex( approxShape ) ) )
+
 
 def main(args):
     rospy.init_node('block_detector')
